3e4ee655ac20bc294c82dd0875a0c818dc7df714
w00990 <wuzixuan@smyfinancial.com>
2020-12-01 14:55:48 +0800

    test Stash

--- src/main/java/com/example/demo/controller/TestA.java
+++ src/main/java/com/example/demo/controller/TestA.java
@@ -1,7 +1,8 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class TestA {
+
 }
