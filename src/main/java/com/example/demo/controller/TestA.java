bc664032b51ccaeb27a33260890291eb6846d65e
w00990 <wuzixuan@smyfinancial.com>
2020-12-01 15:02:55 +0800

    TestAB.java

--- src/main/java/com/example/demo/controller/TestA.java
+++ src/main/java/com/example/demo/controller/TestA.java
@@ -5,5 +5,10 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TestA {
     public  static final String TEST_A = "TEST_A";
+    public static final String TEST_AB="TEST_AB";
+
+    public static void main(String[] args) {
+
+    }
 
 }
--- src/main/java/com/example/demo/controller/TestB.java
+++ src/main/java/com/example/demo/controller/TestB.java
@@ -4,4 +4,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class TestB {
+    public static final String TEST_B="TEST_B";
+    public static final String TEST_AB="TEST_AB";
+
+    public static void main(String[] args) {
+
+    }
 }
