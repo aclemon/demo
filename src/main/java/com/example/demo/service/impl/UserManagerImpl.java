7fd1c75ccb91a659a0e3c5c5ddf52658adefc9d1
w00990 <wuzixuan@smyfinancial.com>
2020-12-01 14:13:13 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/service/impl/UserManagerImpl.java
@@ -0,0 +1,19 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.UserManager;
+
+public class UserManagerImpl implements UserManager {
+    //重写新增用户方法
+    @Override
+    public void addUser(String userName, String password) {
+        System.out.println("调用了新增的方法！");
+        System.out.println("传入参数为 userName: "+userName+" password: "+password);
+    }
+    //重写删除用户方法
+    @Override
+    public void delUser(String userName) {
+        System.out.println("调用了删除的方法！");
+        System.out.println("传入参数为 userName: "+userName);
+    }
+
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    public void test01(){
+        System.out.println("UUID0=>"+ UUID.randomUUID().toString().replaceAll("\\-", ""));
+        System.out.println("UUID1=>"+ UUID.randomUUID().toString().replaceAll("\\-", ""));
+        System.out.println("UUID2=>"+ UUID.randomUUID().toString().replaceAll("\\-", ""));
+        System.out.println("UUID3=>"+ UUID.randomUUID().toString().replaceAll("\\-", ""));
+        int a = 2;
+        if (a==2){
+            System.out.println("UUID4=>"+ UUID.randomUUID().toString().replaceAll("\\-", ""));
+        }
+
+
+    }
+
+}
+
