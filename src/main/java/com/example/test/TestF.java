b16c907a1241bd53cf1f63efe4d209df61dfc02c
aclemon <liunianjinghao07@gmail.com>
2020-12-20 17:03:33 +0800

    编码问题

--- src/main/java/com/example/test/TestF.java
+++ src/main/java/com/example/test/TestF.java
@@ -1,11 +1,14 @@
 package com.example.test;
 
+import cn.hutool.core.lang.Console;
+
 /**
  * @Author: w00990
  * @Date: 2020/12/9
  */
 public class TestF {
     public static void main(String[] args) {
+        Console.log("我爱你=>");
 
     }
 }
